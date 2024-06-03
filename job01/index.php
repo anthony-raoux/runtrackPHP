a8c801e2e92247acdc6c6c16c102f3b6786794d8
anthony-raoux <anthony.raoux@laplateforme.io>
2024-06-03 10:25:39 +0200

    job01 fini

---
+++ job01/index.php
@@ -0,0 +1,20 @@
+<?php
+$variable1 = "LaPlateforme";
+echo $variable1 . "<br>";
+
+$variable2 = "Vive";
+$variable3 = "!";
+echo $variable2 . " " . $variable1 . " " . $variable3 . "<br>";
+
+$number = 6;
+echo $number . "<br>";
+
+$number += 4;
+echo $number . "<br>";
+
+$boolean = true;
+echo $boolean . "<br>"; 
+
+$boolean = false;
+echo $boolean . "<br>"; 
+?>
